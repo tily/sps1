cb603d0210a528c84202bdae7a85bf398155e633
tily <tily05@gmail.com>
2015-02-09 00:57:03 +0900

    add study: 20150209sin

---
+++ javascripts/20150209sin.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  var angle, context, data, pause, play, playing, plot, plotter, processor, source, x;
+  plotter = new Plotter($('canvas').get(0));
+  context = new AudioContext();
+  source = context.createBufferSource();
+  processor = context.createScriptProcessor(4096, 1, 1);
+  playing = true;
+  data = [];
+  x = 0;
+  angle = 2 * Math.PI * 440 / 44100;
+  processor.onaudioprocess = function(e) {
+    var i, _i, _ref, _results;
+    data = e.outputBuffer.getChannelData(0);
+    _results = [];
+    for (i = _i = 0, _ref = e.outputBuffer.length; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+      data[i] = Math.sin(x);
+      _results.push(x += angle);
+    }
+    return _results;
+  };
+  $('canvas').click(function() {
+    if (playing) {
+      pause();
+    } else {
+      play();
+    }
+    return playing = !playing;
+  });
+  play = function() {
+    source.connect(processor);
+    processor.connect(context.destination);
+    return source.start();
+  };
+  pause = function() {
+    processor.disconnect();
+    return source.disconnect();
+  };
+  play();
+  plot = function() {
+    console.log('plot');
+    plotter.clear();
+    plotter.plot(data);
+    return window.requestAnimationFrame(plot);
+  };
+  return window.requestAnimationFrame(plot);
+});
