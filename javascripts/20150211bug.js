40212b065b4afef30133afe816ce673504499c48
tily <tily05@gmail.com>
2015-02-11 10:22:28 +0900

    add study: 20150211bug

---
+++ javascripts/20150211bug.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  var context, data, pause, play, playing, plot, plotter, processor, source, x;
+  plotter = new Plotter($('canvas').get(0));
+  context = new AudioContext();
+  source = context.createBufferSource();
+  processor = context.createScriptProcessor(4096, 1, 1);
+  playing = true;
+  data = [];
+  x = 0;
+  processor.onaudioprocess = function(e) {
+    var i, _i, _ref, _results;
+    data = e.outputBuffer.getChannelData(0);
+    _results = [];
+    for (i = _i = 0, _ref = e.outputBuffer.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      data[i] += Math.sin(2 * Math.PI * x * 440 / 44100);
+      _results.push(x++);
+    }
+    return _results;
+  };
+  $('canvas').click(function() {
+    if (playing) {
+      pause();
+    } else {
+      play();
+    }
+    return playing = !playing;
+  });
+  play = function() {
+    source.connect(processor);
+    return processor.connect(context.destination);
+  };
+  pause = function() {
+    processor.disconnect();
+    return source.disconnect();
+  };
+  play();
+  plot = function() {
+    plotter.clear();
+    plotter.plot(data);
+    return window.requestAnimationFrame(plot);
+  };
+  return window.requestAnimationFrame(plot);
+});
