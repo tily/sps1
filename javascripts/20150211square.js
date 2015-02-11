e3c80d314c959e83cce395cb5cf3eabf3bfe2e22
tily <tily05@gmail.com>
2015-02-11 21:34:02 +0900

    modify: 20150211square

--- javascripts/20150211square.js
+++ javascripts/20150211square.js
@@ -8,14 +8,17 @@ $(document).ready(function() {
   data = [];
   x = 0;
   processor.onaudioprocess = function(e) {
-    var i, j, y, _i, _j, _ref;
+    var i, j, y, _i, _j, _k, _ref, _ref1;
     data = e.outputBuffer.getChannelData(0);
-    for (j = _i = 1; _i < 44; j = ++_i) {
+    for (i = _i = 0, _ref = e.outputBuffer.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      data[i] = 0;
+    }
+    for (j = _j = 1; _j < 44; j = ++_j) {
       if (j % 2 === 0) {
         continue;
       }
       y = x;
-      for (i = _j = 0, _ref = e.outputBuffer.length; 0 <= _ref ? _j < _ref : _j > _ref; i = 0 <= _ref ? ++_j : --_j) {
+      for (i = _k = 0, _ref1 = e.outputBuffer.length; 0 <= _ref1 ? _k < _ref1 : _k > _ref1; i = 0 <= _ref1 ? ++_k : --_k) {
         data[i] += 1.0 / j * Math.sin(2 * Math.PI * y * j * 440 / 44100);
         y++;
       }
