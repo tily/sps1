40d6e8872075a08afcb3fef47e0067fb7633bc71
tily <tily05@gmail.com>
2015-03-02 00:13:52 +0900

    update

--- public/coffeescripts/20150301decibel.coffee
+++ public/coffeescripts/20150301decibel.coffee
@@ -1,22 +1,49 @@
 
+class DecibelMeter
+	constructor: (canvas)->
+		@canvas = canvas
+		@canvas.width = window.innerWidth
+		@canvas.height = window.innerHeight
+		@context = @canvas.getContext('2d')
+		@context.fillRect(0, 50, 50, 50)
+	set: (decibel)->
+		@context.clearRect(0, 0, @canvas.width, @canvas.height)
+		unitWidth = @canvas.width / 63
+		for i in [0..62]
+			if decibel + 60 > i
+				if i > 60
+					@context.fillStyle = "red"
+				else
+					@context.fillStyle = "blue"
+				@context.fillRect(i * unitWidth - 5, 0, unitWidth + 5, @canvas.height)
+
+		#@canvas.drawRect(0, 0, 50, 50)
 $(document).ready ()->
-	plotter = new Plotter($('canvas').get(0))
+	decibelMeter = new DecibelMeter($('canvas').get(0))
 	context = new AudioContext()
 	source = context.createBufferSource()
 	processor = context.createScriptProcessor(4096, 1, 1)
 	stream = null
 	playing = true
 	data = []
+	decibel = -60
 	navigator.webkitGetUserMedia {audio: true}, (stream)->
 		console.log 'microphone ready'
 		stream = context.createMediaStreamSource(stream)
 		stream.connect(processor)
 		processor.connect(context.destination)
-		processor.onaudioprocess = (e)->
-			data = e.inputBuffer.getChannelData(0)
 	, (e)-> console.log e
-	plot = ()->
-		plotter.clear()
-		plotter.plot data
-		window.requestAnimationFrame(plot)
-	window.requestAnimationFrame(plot)
+	# http://stackoverflow.com/questions/13734710/is-there-a-way-get-something-like-decibel-levels-from-an-audio-file-and-transfor
+	processor.onaudioprocess = (e)->
+		input = e.inputBuffer.getChannelData(0)
+		len = input.length   
+		total = i = 0
+		while i < len
+			total += Math.abs( input[i++] )
+	 	rms = Math.sqrt(total / len)
+		decibel = Math.round (20 * (Math.log(rms) / Math.log(10)))
+	draw = ()->
+		console.log decibel
+		window.requestAnimationFrame(draw)
+		decibelMeter.set(decibel)
+	window.requestAnimationFrame(draw)
--- public/javascripts/20150301decibel.js
+++ public/javascripts/20150301decibel.js
@@ -1,29 +1,73 @@
+var DecibelMeter;
+
+DecibelMeter = (function() {
+  function DecibelMeter(canvas) {
+    this.canvas = canvas;
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.context = this.canvas.getContext('2d');
+    this.context.fillRect(0, 50, 50, 50);
+  }
+
+  DecibelMeter.prototype.set = function(decibel) {
+    var i, unitWidth, _i, _results;
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    unitWidth = this.canvas.width / 63;
+    _results = [];
+    for (i = _i = 0; _i <= 62; i = ++_i) {
+      if (decibel + 60 > i) {
+        if (i > 60) {
+          this.context.fillStyle = "red";
+        } else {
+          this.context.fillStyle = "blue";
+        }
+        _results.push(this.context.fillRect(i * unitWidth - 5, 0, unitWidth + 5, this.canvas.height));
+      } else {
+        _results.push(void 0);
+      }
+    }
+    return _results;
+  };
+
+  return DecibelMeter;
+
+})();
+
 $(document).ready(function() {
-  var context, data, playing, plot, plotter, processor, source, stream;
-  plotter = new Plotter($('canvas').get(0));
+  var context, data, decibel, decibelMeter, draw, playing, processor, source, stream;
+  decibelMeter = new DecibelMeter($('canvas').get(0));
   context = new AudioContext();
   source = context.createBufferSource();
   processor = context.createScriptProcessor(4096, 1, 1);
   stream = null;
   playing = true;
   data = [];
+  decibel = -60;
   navigator.webkitGetUserMedia({
     audio: true
   }, function(stream) {
     console.log('microphone ready');
     stream = context.createMediaStreamSource(stream);
     stream.connect(processor);
-    processor.connect(context.destination);
-    return processor.onaudioprocess = function(e) {
-      return data = e.inputBuffer.getChannelData(0);
-    };
+    return processor.connect(context.destination);
   }, function(e) {
     return console.log(e);
   });
-  plot = function() {
-    plotter.clear();
-    plotter.plot(data);
-    return window.requestAnimationFrame(plot);
+  processor.onaudioprocess = function(e) {
+    var i, input, len, rms, total;
+    input = e.inputBuffer.getChannelData(0);
+    len = input.length;
+    total = i = 0;
+    while (i < len) {
+      total += Math.abs(input[i++]);
+      rms = Math.sqrt(total / len);
+    }
+    return decibel = Math.round(20 * (Math.log(rms) / Math.log(10)));
   };
-  return window.requestAnimationFrame(plot);
+  draw = function() {
+    console.log(decibel);
+    window.requestAnimationFrame(draw);
+    return decibelMeter.set(decibel);
+  };
+  return window.requestAnimationFrame(draw);
 });
